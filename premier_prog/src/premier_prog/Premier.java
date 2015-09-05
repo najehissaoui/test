6e4459f4c7f93bd5bbc7c561eaddeb9e299d4b1a
issaoui <issaoui@heni-PC>
2015-09-05 12:57:42 +0100

    ajout d'un nouveau affichage: Bonjour 2

---
+++ premier_prog/src/premier_prog/Premier.java
@@ -0,0 +1,26 @@
+package premier_prog;
+
+public class Premier {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Bonjour");
+    System.out.println("Bonjour2");
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+  }
+
+}
